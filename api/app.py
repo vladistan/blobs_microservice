8df6d21d8df94d0d423a7a669399aaebcb13281b
Vlad <vlad@v-lad.org>
2016-03-20 06:38:42 -0400

    Handle retrieval from cache

--- api/app.py
+++ api/app.py
@@ -1,14 +1,16 @@
+
 from flask import Flask
 from redis import Redis
 from flask import request, redirect
-from werkzeug import secure_filename
 import tempfile
-import base64
 import os
 from  blob_api import blob_api
+import base64
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
+
+base_url = "http://172.16.2.170:8080"
 
 
 @app.route('/blob/v1/count')
@@ -17,27 +19,32 @@ def hello():
     return 'This was called %s times.' % redis.get('hits')
 
 
+@app.route('/blob/v1/obj/<url>', methods=['GET', 'POST'])
 @app.route('/blob/v1/obj/<url>/<idx>', methods=['GET', 'POST'])
-def get_obj(url, idx=1):
+def get_obj(url, idx='1'):
     d_url = base64.b64decode(url)
     app.logger.debug('URL ' + d_url + " IDX " + idx)
     app.logger.debug('Request method ' + request.method)
 
     if request.method == 'GET':
-        return redirect(d_url, 301)
+        obj_location = redis.get(url)
+        if obj_location is None:
+            return redirect(d_url, 302)
+        else:
+            app.logger.debug('Rdir Obj ' + obj_location)
+            return redirect(base_url + obj_location, 301)
     elif request.method == 'POST':
-        accept_incoming_file()
+        accept_incoming_file(url)
         return 'OK. Received'
     else:
         return 'Hmm.. wrong method'
 
 
-def accept_incoming_file():
+def accept_incoming_file(url):
     files = request.files.items()
     for name, file in files:
         app.logger.debug('File :' + name)
-        save_file = secure_filename(name)
-        app.logger.debug('Secure file :' + save_file)
+
         (hndl, tmpname) = tempfile.mkstemp(prefix='/store/tmp/')
         app.logger.debug('TmpFile  %s hndl %d ' % (name, hndl))
         os.close(hndl)
@@ -50,8 +57,10 @@ def accept_incoming_file():
 
         blob_api.make_store_dirs('/store', stored_file_prefix)
         dest_file = os.path.join('/store', stored_file_prefix, file_sum + ".jpeg")
-        app.logger.debug('Dest file :' + dest_file )
+        app.logger.debug('Dest file :' + dest_file)
+
         os.rename(tmpname, dest_file)
+        redis.set(url, dest_file)
 
 
 if __name__ == "__main__":
